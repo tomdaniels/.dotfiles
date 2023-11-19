53b0292b316e602d9a3ae6d96689fe607df05f0a
tdtele <thomsondaniels@hotmail.com>
2023-11-20 07:08:23 +1100

    quit tree on open

--- nvim/.config/nvim/after/plugin/nvim-tree.lua
+++ nvim/.config/nvim/after/plugin/nvim-tree.lua
@@ -5,20 +5,21 @@ vim.g.loaded_netrwPlugin = 1
 -- set termguicolors to enable highlight groups
 vim.opt.termguicolors = true
 
--- empty setup using defaults
-require("nvim-tree").setup()
-
--- OR setup with some options
 require("nvim-tree").setup({
   sort_by = "case_sensitive",
   view = {
-    width = 30,
+    width = 35,
   },
   renderer = {
     group_empty = true,
   },
   filters = {
     dotfiles = true,
+  },
+  actions = {
+    open_file = {
+      quit_on_open = true,
+    },
   },
 })
 
