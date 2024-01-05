c2832a11f9d8d2d9382470337b565732568fe729
tdtele <thomsondaniels@hotmail.com>
2024-01-06 09:46:17 +1100

    autopairs & commenter

--- nvim/.config/nvim/lua/custom/plugins.lua
+++ nvim/.config/nvim/lua/custom/plugins.lua
@@ -1,20 +1,17 @@
 return {
-  { 'sainnhe/gruvbox-material' },
-  { 'f-person/git-blame.nvim' },
-  { 'nvim-treesitter/playground' },
-  { 'mbbill/undotree' },
-  { 'theprimeagen/harpoon' },
-  { 'wakatime/vim-wakatime' },
-  { 'neovim/nvim-lspconfig' },
-  { 'neoclide/coc.nvim', branch = 'release'},
-  { 'nvim-tree/nvim-tree.lua' },
-  {
-    "jose-elias-alvarez/buftabline.nvim",
-    requires = {"kyazdani42/nvim-web-devicons"},
-    config = function() require("buftabline").setup {} end
-  },
+  { "sainnhe/gruvbox-material" },
+  { "f-person/git-blame.nvim" },
+  { "nvim-treesitter/playground" },
+  { "mbbill/undotree" },
+  { "theprimeagen/harpoon" },
+  { "wakatime/vim-wakatime" },
+  { "neovim/nvim-lspconfig" },
+  { "neoclide/coc.nvim", branch = "release"},
+  { "nvim-tree/nvim-tree.lua" },
   { "startup-nvim/startup.nvim" },
-  { "norcalli/nvim-colorizer.lua" }
+  { "norcalli/nvim-colorizer.lua" },
+  { "windwp/nvim-autopairs", event = "InsertEnter", opts = {}},
+  { "terrortylor/nvim-comment" },
 }
 
 
