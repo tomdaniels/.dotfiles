d3ab33f74844e3abba7b5ce3b000301135e75212
tdtele <thomsondaniels@hotmail.com>
2024-06-02 00:08:25 +1000

    fish: refactor gc branch/ticket inference

--- config/fish/functions/gc.fish
+++ config/fish/functions/gc.fish
@@ -1,20 +1,16 @@
 function gc --wraps='git commit'
     set branch_name (git rev-parse --abbrev-ref HEAD)
-    set ticket (string match -r "(?:PKB)-[^-]*" $branch_name)
-    set inferred_branch_type (string split '/' $branch_name)[1]
     
     set branch_type ""
     set commit_message ""
     switch (count $argv)
         case 1
-            # if there's no "/" in the branch we can't infer the name, so both arguments are required.
-            if test -z (string match -r '/' $branch_name); 
-                throw "Unable to infer branch type for a semantic commit, please supplu both arguments. gc [branch_type] [commit_message]"
+            if test -z (string match -r '/' $branch_name)
+                throw "Unable to infer branch type for a semantic commit, please supply both arguments. gc [branch_type] [commit_message]"
                 return 1
             end
-
             set commit_message $argv[1]
-            set branch_type $inferred_branch_type
+            set branch_type (infer_branch_type $branch_name)
         case 2
             set branch_type $argv[1]
             set commit_message $argv[2]
@@ -28,12 +24,12 @@ function gc --wraps='git commit'
         return 1
     end
     
-    # Check if either branch_type is empty or branch name doesn't contain '/'
     if test -z "$branch_type"
         throw "Missing branch type. If your branch is not prefixed correctly please supply the arguments: gc [branch_type] [commit_message]"
         return 1
     end
     
+    set ticket (infer_ticket $branch_name)
     if test -z "$ticket"
         git commit -m "$branch_type: $commit_message"
         return 0
@@ -41,8 +37,21 @@ function gc --wraps='git commit'
     git commit -m "$branch_type($ticket): $commit_message"
 end
 
+function infer_branch_type
+    set branch_name $argv[1]
+    set inferred_branch_type (string split '/' $branch_name)[1]
+    echo $inferred_branch_type
+end
+
+function infer_ticket
+    set branch_name $argv[1]
+    set ticket (string match -r "(?:PKB)-[^-]*" $branch_name)
+    echo $ticket
+end
+
 function throw
     printf "%sInvalid Format%s:\n" (set_color red) (set_color normal)
     echo $argv[1]     
     return 1
 end
+
