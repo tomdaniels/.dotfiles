01b80595c858a3e6ff6f2afddac57d058e5f835c
tdtele <thomsondaniels@hotmail.com>
2024-06-02 21:06:15 +1000

    fish: trail command aliases

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -1,7 +1,6 @@
 if status is-interactive
 	fish_config theme choose Base16\ Eighties
 
-	bind \e 'tmux copy-mode'
 	# bindings for tmux nav, need both Normal and Insert mode
 	bind \cf $HOME/.dotfiles/scripts/tmux/tmux-nav
 	bind -M insert \cf $HOME/.dotfiles/scripts/tmux/tmux-nav
@@ -11,6 +10,10 @@ if status is-interactive
 	alias vc="v ~/.config/nvim/init.lua"
 	set -gx EDITOR nvim
 	fish_vi_key_bindings
+
+	# trial
+	# bind \e 'tmux copy-mode' 
+	alias gs="git status -s"
 
 	# cargo (rust)
 	set -gx PATH $HOME/.cargo/bin $PATH
