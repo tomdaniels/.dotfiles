d582dae6f66eec34c62cda7a14ee64028e4b676a
tdtele <thomsondaniels@hotmail.com>
2024-06-25 16:50:14 +1000

    nvim: dont update treesitter every time we start

--- config/nvim/lua/tdtele/lazy/treesitter.lua
+++ config/nvim/lua/tdtele/lazy/treesitter.lua
@@ -1,6 +1,6 @@
 return {
     "nvim-treesitter/nvim-treesitter",
-    build = ":TSUpdate",
+    -- build = ":TSUpdate",
     config = function()
         ---@diagnostic disable: missing-fields
         require("nvim-treesitter.configs").setup({
