95cd090b00c78bdd912c35f1c202f755f3792e82
tdtele <thomsondaniels@hotmail.com>
2024-06-26 16:00:29 +1000

    nvim(keymaps): cycle through buffers

--- config/nvim/lua/tdtele/keymaps.lua
+++ config/nvim/lua/tdtele/keymaps.lua
@@ -1,3 +1,6 @@
+vim.keymap.set("n", "<tab>",     ":bn<CR>", { desc = "Next Buffer" })
+vim.keymap.set("n", "<S-tab>",   ":bp<CR>", { desc = "Previous Buffer" })
+
 -- move lines by visual selection
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
