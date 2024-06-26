e4acb88a572b7aaa2723e9c6dc518b4dc72b915c
tdtele <thomsondaniels@hotmail.com>
2024-06-26 16:11:28 +1000

    nvim(telescope): rmv useless comments

--- config/nvim/lua/tdtele/lazy/telescope.lua
+++ config/nvim/lua/tdtele/lazy/telescope.lua
@@ -22,11 +22,9 @@ return {
     vim.keymap.set('n', '<leader>sg', require('telescope.builtin').live_grep,   { desc = '[S]earch by [G]rep'      })
     vim.keymap.set('n', '<leader>sd', require('telescope.builtin').diagnostics, { desc = '[S]earch [D]iagnostics'  })
 
-    -- See `:help telescope.builtin`
     vim.keymap.set('n', '<leader>?', require('telescope.builtin').oldfiles, { desc = '[?] Find recently opened files' })
     vim.keymap.set('n', '<leader><space>', require('telescope.builtin').buffers, { desc = '[ ] Find existing buffers' })
     vim.keymap.set('n', '<leader>/', function()
-      -- You can pass additional configuration to telescope to change theme, layout, etc.
       require('telescope.builtin').current_buffer_fuzzy_find(require('telescope.themes').get_dropdown {
         winblend  = 10,
         previewer = false,
