9b5d39a4f3e04bd2184ec89daf40d63b0a01fbda
tdtele <thomsondaniels@hotmail.com>
2024-06-28 11:53:26 +1000

    nvim: fix tstools dependencies

--- config/nvim/lua/tdtele/lazy/typescript-tools.lua
+++ config/nvim/lua/tdtele/lazy/typescript-tools.lua
@@ -8,6 +8,7 @@ end
 
 return {
  "pmizio/typescript-tools.nvim",
+  dependencies = { "nvim-lua/plenary.nvim", "neovim/nvim-lspconfig" },
   config = function()
     require("typescript-tools").setup {
       settings = {
