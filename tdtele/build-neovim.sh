85b2b0734011f7b9525c4299c7fbf0c2c77ccf7d
tdtele <thomsondaniels@hotmail.com>
2024-06-28 11:53:11 +1000

    go all in on fish
    remove z shell config and init set up scripts. Remove legacy scripts dir with all the bash scripts. The ones i used were already migrated over to fish/functions.

---
+++ tdtele/build-neovim.sh
@@ -0,0 +1,4 @@
+cd ~
+git clone https://github.com/neovim/neovim
+cd neovim && make CMAKE_BUILD_TYPE=RelWithDebInfo
+sudo make install
---
+++ tdtele/install-nodejs.sh
@@ -0,0 +1,5 @@
+fisher install jorgebucaran/nvm.fish
+nvm install latest
+nvm use latest
+npm i -g cowsay
+npm i -g typescript
