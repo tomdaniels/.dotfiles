9e91b30ec81b82c3cf9450e5641b22b468f0dfa4
tdtele <thomsondaniels@hotmail.com>
2024-06-28 12:12:45 +1000

    dos2unix

--- tdtele/welcome.sh
+++ tdtele/welcome.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
-
 message="Hello, $USER."
-opts=("homer" "elephant2" "catfence" "-r") 
+opts=("homer" "elephant2" "catfence" "-r")
 opt="${opts[RANDOM % ${#opts[@]}]}"
 
 cowsay -f "$opt" "$message" | lolcat
