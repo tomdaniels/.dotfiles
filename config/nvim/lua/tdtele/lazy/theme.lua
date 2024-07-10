6f43a0efe33252b0f03a5ad07e0560bb211ffdcb
tdtele <thomsondaniels@hotmail.com>
2024-07-11 09:31:00 +1000

    nvim(theme): gray unused vars

--- config/nvim/lua/tdtele/lazy/theme.lua
+++ config/nvim/lua/tdtele/lazy/theme.lua
@@ -2,5 +2,15 @@ return {
   "sainnhe/gruvbox-material",
   config = function()
     vim.cmd [[ colorscheme gruvbox-material ]]
+  end,
+
+  init = function ()
+    vim.api.nvim_create_autocmd({"ColorScheme"}, {
+      group = tdtele,
+      pattern = "*",
+      callback = function ()
+        vim.api.nvim_set_hl(0, "DiagnosticUnnecessary", { link = "Comment" })
+      end
+    })
   end
 }
