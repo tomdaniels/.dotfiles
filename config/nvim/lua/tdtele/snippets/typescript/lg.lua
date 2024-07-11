69be3249336a3dbf8ded1eabfe4d7ffe74011279
tdtele <thomsondaniels@hotmail.com>
2024-07-11 17:38:18 +1000

    nvim(snippet): fix dev log snippet

--- config/nvim/lua/tdtele/snippets/typescript/lg.lua
+++ config/nvim/lua/tdtele/snippets/typescript/lg.lua
@@ -1,15 +1,15 @@
 return {
   s({
-    trig = "dlog",
+    trig = "lg",
     name = "Insert debug console log",
     desc = "Inserts a console.log() with @@@ prefixed for debugging",
   }, {
     t("console.log(\"@@@"),
-    i(1),
-    t("\", "),
     f(function(args)
       return args[1][1]
     end, { 1 }),
+    t("\", "),
+    i(1),
     t(");")
   }),
 }
