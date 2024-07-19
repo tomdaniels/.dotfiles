ff701e0faa1488df197d34d20ef34d34539741cf
tdtele <thomsondaniels@hotmail.com>
2024-07-19 10:12:09 +1000

    nvim(telescope): update keybinds

--- config/nvim/lua/tdtele/lazy/telescope.lua
+++ config/nvim/lua/tdtele/lazy/telescope.lua
@@ -14,13 +14,13 @@ return {
   },
 
   init = function ()
-    vim.keymap.set('n', '<C-p>',      require('telescope.builtin').find_files,  { desc = '[S]earch [F]iles'        })
-    vim.keymap.set('n', '<leader>sf', require('telescope.builtin').find_files,  { desc = '[S]earch [F]iles'        })
-    vim.keymap.set('n', '<leader>gf', require('telescope.builtin').git_files,   { desc = 'Search [G]it [F]iles'    })
-    vim.keymap.set('n', '<leader>sh', require('telescope.builtin').help_tags,   { desc = '[S]earch [H]elp',        })
-    vim.keymap.set('n', '<leader>sw', require('telescope.builtin').grep_string, { desc = '[S]earch current [W]ord' })
-    vim.keymap.set('n', '<leader>sg', require('telescope.builtin').live_grep,   { desc = '[S]earch by [G]rep'      })
-    vim.keymap.set('n', '<leader>sd', require('telescope.builtin').diagnostics, { desc = '[S]earch [D]iagnostics'  })
+    vim.keymap.set('n', '<C-p>',      require('telescope.builtin').find_files) -- shortcut
+    vim.keymap.set('n', '<leader>lf', require('telescope.builtin').find_files,  { desc = '[L]ook [F]or (files)'         })
+    vim.keymap.set('n', '<leader>lF', require('telescope.builtin').live_grep,   { desc = '[L]ook [F]or (text in files)' })
+    vim.keymap.set('n', '<leader>sw', require('telescope.builtin').grep_string, { desc = '[S]earch [W]ord under cursor' })
+    vim.keymap.set('n', '<leader>sg', require('telescope.builtin').git_files,   { desc = '[S]earch [G]it files'         })
+    vim.keymap.set('n', '<leader>sd', require('telescope.builtin').diagnostics, { desc = '[S]earch [D]iagnostics'       })
+    vim.keymap.set('n', '<leader>sh', require('telescope.builtin').help_tags,   { desc = '[S]earch [H]elp',             })
 
     vim.keymap.set('n', '<leader>?', require('telescope.builtin').oldfiles, { desc = '[?] Find recently opened files' })
     vim.keymap.set('n', '<leader><space>', require('telescope.builtin').buffers, { desc = '[ ] Find existing buffers' })
