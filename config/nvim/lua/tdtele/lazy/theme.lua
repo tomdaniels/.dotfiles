125cb7b9766157a6229bc5a0fc92a6dd5ecf3ee0
tdtele <thomsondaniels@hotmail.com>
2024-07-22 09:53:28 +1000

    nvim: theme enhancements

--- config/nvim/lua/tdtele/lazy/theme.lua
+++ config/nvim/lua/tdtele/lazy/theme.lua
@@ -9,6 +9,10 @@ return {
       group = tdtele,
       pattern = "*",
       callback = function ()
+        vim.api.nvim_set_hl(0, "@tag.tsx", { link = "Yellow" })
+        vim.api.nvim_set_hl(0, "@tag.builtin.tsx", { link = "Orange" })
+        vim.api.nvim_set_hl(0, "@character.special.scss", { link = "Orange" })
+        vim.api.nvim_set_hl(0, "@property.scss", { link = "Blue" })
         vim.api.nvim_set_hl(0, "DiagnosticUnnecessary", { link = "Comment" })
       end
     })
