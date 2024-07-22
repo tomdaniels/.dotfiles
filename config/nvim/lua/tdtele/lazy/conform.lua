83704763d3162ca21f6f163db07726052637926c
tdtele <thomsondaniels@hotmail.com>
2024-07-23 09:46:03 +1000

    nvim(conform): allow biome auto formatting

--- config/nvim/lua/tdtele/lazy/conform.lua
+++ config/nvim/lua/tdtele/lazy/conform.lua
@@ -1,14 +1,17 @@
 return {
   "stevearc/conform.nvim",
+  event = { "BufReadPre", "BufNewFile" },
 
   config = function()
+    local ts_formatters = { "biome", "prettier", "prettierd", stop_after_first = true }
+
     require("conform").setup({
       formatters_by_ft = {
         lua = { "stylua" },
         python = { "isort", "black" },
         rust = { "rustfmt", lsp_format = "fallback" },
-        typescript = { "prettierd", "prettier", stop_after_first = true },
-        typescriptreact = { "prettierd", "prettier", stop_after_first = true },
+        typescript = ts_formatters,
+        typescriptreact = ts_formatters,
       },
 
       format_on_save = {
