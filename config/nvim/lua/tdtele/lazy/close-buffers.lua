798fa3b3a31eeed3c5736da6fa2415d37214425a
tdtele <thomsondaniels@hotmail.com>
2024-07-23 22:01:56 +1000

    nvim: spec file not needed for buffers

--- config/nvim/lua/tdtele/lazy/close-buffers.lua
+++
@@ -1,7 +0,0 @@
-return {
-  "Asheq/close-buffers.vim",
-  init = function()
-    vim.keymap.set("n", "<leader>d", ":Bdelete other<CR>", { desc = "Close Other Buffers" })
-    vim.keymap.set("n", "<leader>x", ":bdelete<CR>", { desc = "Close Current Buffer" })
-  end,
-}
