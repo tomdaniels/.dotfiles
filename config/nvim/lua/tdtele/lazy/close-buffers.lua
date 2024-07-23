4112f240299fb9c17edbd637851a87f209defffa
tdtele <thomsondaniels@hotmail.com>
2024-07-23 22:00:00 +1000

    nvim: close buffers keymap

--- config/nvim/lua/tdtele/lazy/close-buffers.lua
+++ config/nvim/lua/tdtele/lazy/close-buffers.lua
@@ -1,10 +1,7 @@
 return {
   "Asheq/close-buffers.vim",
   init = function()
-    -- close all other buffers
-    vim.keymap.set("n", "<leader>d", ":Bdelete other<CR>", { desc = "Close (Destroy) Other Buffers" })
-
-    -- close current buffer
+    vim.keymap.set("n", "<leader>d", ":Bdelete other<CR>", { desc = "Close Other Buffers" })
     vim.keymap.set("n", "<leader>x", ":bdelete<CR>", { desc = "Close Current Buffer" })
   end,
 }
