4d01203129081f78ec6e0a2c59b99d75ea26eacc
tdtele <thomsondaniels@hotmail.com>
2026-04-04 20:41:05 +1100

    fix neovim install

--- tdtele/build-neovim.sh
+++ tdtele/build-neovim.sh
@@ -1,4 +1,4 @@
 cd ~
-git clone https://github.com/neovim/neovim
-cd neovim && make CMAKE_BUILD_TYPE=RelWithDebInfo
+git clone https://github.com/neovim/neovim ~/.local/src/neovim
+cd ~/.local/src/neovim && make CMAKE_BUILD_TYPE=RelWithDebInfo
 sudo make install
