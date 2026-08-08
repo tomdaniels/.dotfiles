ea01cafb362baad096d587e2f08063e4f9b77725
tdtele <thomsondaniels@hotmail.com>
2026-08-08 12:06:13 +1000

    chore: nuke nvim

--- config/nvim/lua/tdtele/lazy/autotag.lua
+++
@@ -1,6 +0,0 @@
-return {
-  "nvim-ts-autotag",
-  config = function()
-    require("nvim-ts-autotag").setup()
-  end,
-}
--- config/nvim/lua/tdtele/lazy/undotree.lua
+++
@@ -1,6 +0,0 @@
-return {
-  "mbbill/undotree",
-  keys = {
-    { "<leader>ut", "<cmd>UndotreeToggle<cr>", desc = "NeoTree" },
-  },
-}
