712ab045dac99c23298b2a46463929af4a9c3236
tdtele <thomsondaniels@hotmail.com>
2026-08-08 17:18:58 +1000

    which key

---
+++ config/nvim/lua/tdtele/lazy/which-key.lua
@@ -0,0 +1,22 @@
+return {
+  "folke/which-key.nvim",
+  event = "VeryLazy",
+  opts = {
+    spec = {
+      { "<leader>t", group = "TypeScript" },
+      { "<leader>g", group = "Git" },
+      { "<leader>l", group = "Find" },
+      { "<leader>s", group = "Search" },
+      { "<leader>d", group = "Docs" },
+    },
+  },
+  keys = {
+    {
+      "<leader>k",
+      function()
+        require("which-key").show({ global = false })
+      end,
+      desc = "Buffer Local Keymaps (which-key)",
+    },
+  },
+}
